b0a89b4121832f45029db2d95bcd68dd1dc43d58
vkavith <vkavith@gmail.com>
2024-11-19 00:08:57 -0500

    ALAB308.3.1-Practical-Loops

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*console.log("Part 1 : Fizz Buzz");
+console.log("Part 1 : Fizz Buzz\n ");
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
@@ -11,11 +11,10 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
-*/
 
-console.log("Part 2: ");
+console.log("\nPart 2: Prime Time\n");
 
-console.log("Part 3: Feeling Loopy");
+console.log("Part 3: Feeling Loopy\n");
 
 //Test String
 
@@ -54,7 +53,7 @@ if (cell) {
   console.log(cellArray[0], cellArray[1], cellArray[2], cellArray[3]);
 }
 
-console.log("Test Actual String");
+console.log("\n\nTest Actual String\n\n");
 const csvData2 =
   "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
